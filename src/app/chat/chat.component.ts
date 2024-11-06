ca5fc1a51c5719a7c5cc9237ea8564910aea842e
Sheng Liu <SLiu40@slb.com>
2024-11-06 20:08:54 +0800

    feat:add loading

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,14 +8,15 @@ import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {AliApiService} from "../service/ali-api.service";
 import {MatMiniFabButton} from "@angular/material/button";
 import {MatOption, MatSelect} from "@angular/material/select";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
-    FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatGridList, MatGridTile, MatCardContent, MatCard, MatMiniFabButton, MatSelect, NgForOf, MatOption, MatCardHeader
+    FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatGridList, MatGridTile, MatCardContent, MatCard, MatMiniFabButton, MatSelect, NgForOf, MatOption, MatCardHeader, MatProgressSpinner, NgIf
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
@@ -26,12 +27,15 @@ export class ChatComponent {
   selectedKey:string="高情商"
   inputValue:string="";
   responseValue:string="";
+  isLoading:boolean = false;
 
   request(){
+    this.isLoading=true;
     this.ali.generateContent(this.inputValue,this.selectedKey).subscribe(
       result => {
         console.log(result);
         this.responseValue = result
+        this.isLoading=false;
       }
     );
     console.log(this.selectedKey)
