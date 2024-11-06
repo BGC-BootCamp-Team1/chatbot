b9bae3c8adbd02e0e62cf23120b23328e249f441
Sheng Liu <SLiu40@slb.com>
2024-11-06 19:20:00 +0800

    feat:add role

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -4,16 +4,18 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatIcon} from "@angular/material/icon";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
-import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {AliApiService} from "../service/ali-api.service";
 import {MatMiniFabButton} from "@angular/material/button";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {NgForOf} from "@angular/common";
 
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
-    FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatGridList, MatGridTile, MatCardContent, MatCard, MatMiniFabButton
+    FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatGridList, MatGridTile, MatCardContent, MatCard, MatMiniFabButton, MatSelect, NgForOf, MatOption, MatCardHeader
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
@@ -21,16 +23,18 @@ import {MatMiniFabButton} from "@angular/material/button";
 export class ChatComponent {
   constructor(private ali: AliApiService) {
   }
+  selectedKey:string="高情商"
   inputValue:string="";
   responseValue:string="";
 
   request(){
-    this.ali.generateContent(this.inputValue).subscribe(
+    this.ali.generateContent(this.inputValue,this.selectedKey).subscribe(
       result => {
         console.log(result);
         this.responseValue = result
       }
     );
+    console.log(this.selectedKey)
 
   }
 
--- src/app/service/ali-api.service.ts
+++ src/app/service/ali-api.service.ts
@@ -16,7 +16,7 @@ export class AliApiService {
   constructor(private http: HttpClient) { }
 
   // 定义方法并只返回 content 字段
-  generateContent(content:string): Observable<string> {
+  generateContent(content:string,prompt:string): Observable<string> {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.apiKey}`,
       'Content-Type': 'application/json'
@@ -26,7 +26,7 @@ export class AliApiService {
       "model": "qwen-max",
       "input": {
         "messages": [
-          { "role": "system", "content": "你是一个能够提供情绪价值的高手,你的特点是幽默且充满正能量,总是能够给别人及时的情绪价值,我会给你一些话，你根据这些话给出高情商和简短回复" },
+          { "role": "system", "content": `${prompt}` },
           { "content": `${content}`, "role": "user" }
         ]
       },
